3a36f5ee803e0d02f4ddc0cbaddc62d11fa8f5f5
unknown <ritarixxter@yandex.ru>
2024-01-18 17:33:58 +0300

    Добавила ErrorBoundary

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,12 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
-        jest:true
+        jest: true,
     },
     extends: [
         'plugin:react/recommended',
         'airbnb',
-        'plugin:i18next/recommended',
+        // 'plugin:i18next/recommended',
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
@@ -20,12 +20,13 @@ module.exports = {
     plugins: [
         'react',
         '@typescript-eslint',
-        'i18next',
+        // 'i18next',
     ],
     rules: {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
+        // eslint-disable-next-line max-len
         'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
@@ -38,8 +39,9 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': ['error', { markupOnly: true }],
-        "max-len": ['error', {'ignoreComments':true}]
+        // 'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'max-len': ['error', { ignoreComments: true, code: 100 }],
+        'linebreak-style': ['error', 'windows'],
     },
     globals: {
         __IS_DEV__: true,
