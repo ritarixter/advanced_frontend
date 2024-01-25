115d4384ae4dd8f26297b07633ac0e5d9529646a
ritarixxter <ritarixxter@yandex.tu>
2024-01-25 13:49:02 +0300

    add CI file yml

--- src/pages/NotFoundPage/index.ts
+++ src/pages/NotFoundPage/index.ts
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
 export { NotFoundPage } from './ui/NotFoundPage'
-=======
-import { NotFoundPage } from './ui/NotFoundPage';
-
-export {
-    NotFoundPage,
-};
->>>>>>> 3cdc37cf3b0bb92cef445557af90e5660e3c980b
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -6,20 +6,12 @@ import { NotFoundPage } from 'pages/NotFoundPage';
 export enum AppRoutes {
     MAIN = 'main',
     ABOUT = 'about',
-<<<<<<< HEAD
-    NOT_FOUND = 'not_found'
-=======
     NOT_FOUND = 'not_found',
->>>>>>> 3cdc37cf3b0bb92cef445557af90e5660e3c980b
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: '/',
     [AppRoutes.ABOUT]: '/about',
-<<<<<<< HEAD
-=======
-    // последний
->>>>>>> 3cdc37cf3b0bb92cef445557af90e5660e3c980b
     [AppRoutes.NOT_FOUND]: '*',
 };
 
