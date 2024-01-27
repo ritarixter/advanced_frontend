cb9943bb71ef5654dbfd9d889a4d9499eb35044e
ritarixxter <ritarixxter@yandex.tu>
2024-01-27 21:41:53 +0300

    fix lint error

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -3,12 +3,12 @@ import { Button, ThemeButton } from 'shared/ui/Button/Button';
 
 describe('Button', () => {
     test('Test render', () => {
-        render(<Button>TEST</Button>);
-        expect(screen.getByText('TEST')).toBeInTheDocument();
+        render(<Button>TEST</Button>)
+        expect(screen.getByText('TEST')).toBeInTheDocument()
     });
 
     test('Test clear theme', () => {
-        render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
-        expect(screen.getByText('TEST')).toHaveClass('clear');
+        render(<Button theme={ThemeButton.CLEAR}>TEST</Button>)
+        expect(screen.getByText('TEST')).toHaveClass('clear')
     });
 });
